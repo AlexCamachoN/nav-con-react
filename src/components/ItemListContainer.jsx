da0bc6ee0c233699e0f7172b7a7166e0ea9cf65d
Alex <cnalex_2@hotmail.com>
2022-01-03 22:26:12 -0500

    queriendo agregar carousel

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -8,6 +8,19 @@ export  function ItemListContainer() {
     return (
             <>
                 Carousel nuevo
+                <Carousel fade>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src={bodegoncarrosel}
+                        alt="First slide"
+                        />
+                        <Carousel.Caption>
+                        <h3>First slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
               
                
             </>
